447dfa64bb8982abc44e0ba0ec991b34d52487b1
wgmb745 <66256132+wgmb745@users.noreply.github.com>
2020-06-16 17:04:57 -0500

    Update index.py

--- index.py
+++ index.py
@@ -3,10 +3,33 @@ import datetime
 
 
 def handler(event, context):
+    n = int(0)
+    triangular = int(0)
+    lista = []
+    if event is not None:
+        #n = int(event["queryStringParameters"]["numfib"])
+        body = json.loads(event["body"])
+        n = body['numfib']
+        a, b = 0,1
+        while a < n:
+            lista.append(a)
+            a, b = b, a+b
+            
+        triangular = int((n*(n+1)/2))
+    else:
+        lista.append('No se definio un parametro para inicio')
+        triangular='Sin dato'
+    
     data = {
-        'output': 'Hello World',
+        'numero':n,
+        'fibonacci':lista,
+        'triangular':triangular,
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
+    """data = {
+        'output': 'Hello World',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }"""
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
